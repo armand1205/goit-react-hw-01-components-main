b358453b1683e688678fdedf816d7499f57251b9
armand1205 <munteanuarmandshadow1205@gmail.com>
2024-02-29 01:33:44 +0200

    commit final

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -24,4 +24,15 @@ const Statistics = ({ title, stats }) => {
     );
 };
 
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
+
 export default Statistics;
